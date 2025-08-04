70981a3bfc279e5a618c6020a04c4a0b79363cf1
Yui Fan <yuifan@Yuis-MacBook-Air-4.local>
2025-08-04 12:01:38 +0100

    env adjustment

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,13 @@
+import dotenv from "dotenv";
 import mongoose from "mongoose";
+dotenv.config();
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://ericyuifan:eric6221@cluster0.6a7fxzm.mongodb.net/Food_Delivery').then(()=>console.log('DB Connected'));
-}
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("DB Connected");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1);
+  }
+};
